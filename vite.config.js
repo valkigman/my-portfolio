d079b80ec86acb97accfb982f4ab40cac3f152ba
Valentine Kiguli <kvale8@gmail.com>
2025-03-19 06:45:20 +0300

    Modified the vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: 'terser',
-    sourcemap: false
-  }
+  // build: {
+  //   minify: 'terser',
+  //   sourcemap: false
+  // }
 });
