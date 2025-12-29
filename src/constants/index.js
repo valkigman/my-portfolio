b4a69b8c0c87b608dc462dbc3beb35e946e6110a
Valentine Kiguli <kvale8@gmail.com>
2025-12-29 22:28:28 +0000

    Update index.js
    Corrected error 'gimport'

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-gimport project1 from "../assets/projects/project-1.jpg";
+import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 // import project4 from "../assets/projects/project-4.jpg";
