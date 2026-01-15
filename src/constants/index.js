6f255e1523904f4e5f505818c5d26426f8548ae2
Valentine Kiguli <kvale8@gmail.com>
2026-01-15 23:03:00 +0000

    Update index.js
    housekeeping: remove commented out code

--- src/constants/index.js
+++ src/constants/index.js
@@ -22,20 +22,6 @@ export const EXPERIENCES = [
     description: `•	Collaborated on a machine learning project to develop a solution for detecting potholes on roads using image recognition and data analytics techniques.`,
     technologies: ["Python", "Jupyter Notebook", "FastAI", "PyTorch"],
   },
-  // {
-  //   year: "2021 - 2022",
-  //   role: "Full Stack Developer",
-  //   company: "Facebook",
-  //   description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-  //   technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  // },
-  // {
-  //   year: "2020 - 2021",
-  //   role: "Software Engineer",
-  //   company: "Paypal",
-  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  // },
 ];
 
 export const PROJECTS = [
@@ -60,13 +46,6 @@ export const PROJECTS = [
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
-  // {
-  //   title: "Blogging Platform",
-  //   image: project4,
-  //   description:
-  //     "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-  //   technologies: ["HTML", "CSS", "Vue.js", "Express", "MySQL"],
-  // },
 ];
 
 export const CONTACT = {
